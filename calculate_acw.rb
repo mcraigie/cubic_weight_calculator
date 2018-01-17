c193b35e32bc2924937e164bed06f70e6eefde48
mcraigie <max.craigie@gmail.com>
2018-01-17 11:30:56 +1100

    add main loop and page retriever method

--- calculate_acw.rb
+++ calculate_acw.rb
@@ -52,6 +52,18 @@ def validate_configuration_values(config)
   abort('Error: Must specify a category') if config[:categories].empty?
 end
 
+def retrieve_page(http, page_path)
+  response = http.request(Net::HTTP::Get.new(page_path)).body
+  JSON.parse(response, symbolize_names: true)
+rescue JSON::ParserError => e
+  abort("Error: Unable to parse page\n#{e.message}")
+rescue StandardError => e
+  # Rescuing StandardError isn't usually a good idea. In this case it is a trade-off between
+  # verbosity (since Net::HTTP can throw a very large variety of errors) and the slim chance that
+  # something unexpected will occur when retrieving a page that shouldn't also be a trigger to abort
+  abort("Error: Unable to retrieve page\n#{e.message}")
+end
+
 def cubic_weight(product) # in grams
   size = product[:size]
   size.empty? ? 0 : size.values.reduce(:*) * (CONFIG[:conversion_factor_kg_m3] / 1000.0)
@@ -59,4 +71,17 @@ end
 
 def average(array)
   array.sum / array.length.to_f
+end
+
+CONFIG = load_configuration
+next_page_path = CONFIG[:starting_page]
+cubic_weights = []
+
+# maintain a single connection to the API backend (reduces overhead when making mutliple queries)
+Net::HTTP.start(URI(CONFIG[:api_host]).host) do |http|
+  while next_page_path
+    page = retrieve_page(http, next_page_path)
+    next_page_path = page[:next]
+    cubic_weights.concat(products.map { |product| cubic_weight(product) })
+  end
 end
