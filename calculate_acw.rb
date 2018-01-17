4268f302cb40f1bb5c09498baeabce37d7153bd2
mcraigie <max.craigie@gmail.com>
2018-01-17 11:26:55 +1100

    add validator for presence of config options

--- calculate_acw.rb
+++ calculate_acw.rb
@@ -17,3 +17,21 @@ def load_configuration
 
   config
 end
+
+def validate_configuration_presence(config)
+  required = %i[
+    api_host
+    categories
+    include_sizeless
+    conversion_factor_kg_m3
+    result_rounding
+    result_unit
+    starting_page
+  ]
+
+  missing = required - config.keys
+  abort("Error: Missing configuration options: (#{missing})") unless missing.empty?
+
+  unused = config.keys - required
+  abort("Error: Unused configuration options: (#{unused})") unless unused.empty?
+end
